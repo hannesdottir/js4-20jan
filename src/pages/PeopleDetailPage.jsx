9218d4e974d6aa1475e888e8e4c3d0b84360da9f
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2021-01-20 11:46:08 +0100

    add to PeopleDetailPage to get right name with id

--- src/pages/PeopleDetailPage.jsx
+++ src/pages/PeopleDetailPage.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function PeopleDetailPage() {
-  return <div>People Detail Page</div>;
+export default function PeopleDetailPage(props) {
+  const id = props.match.params.id;
+  const [person, setPerson] = useState(null);
+
+  function getPerson() {
+    const url = `https://swapi.dev/api/people/${id}/`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPerson(data));
+  }
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  return (
+    <div>
+      People Detail Page
+      {person && <h1>{person.name}</h1>}
+    </div>
+  );
 }
